98ced3ccbb661df1ea91c8b9092d4c7d8d75c690
TotooriaHyperion <shr9192@gmail.com>
2019-05-07 20:18:41 +0800

    implement useMemo, useCallback, useContext. fix HMR causing multiple hooks register.

--- index.js
+++ index.js
@@ -1,177 +1,215 @@
-let currentInstance = null
-let isMounting = false
-let callIndex = 0
+let currentInstance = null;
+let isMounting = false;
+let callIndex = 0;
 
 function ensureCurrentInstance() {
   if (!currentInstance) {
     throw new Error(
-      `invalid hooks call: hooks can only be called in a function passed to withHooks.`
-    )
+      `invalid hooks call: hooks can only be called in a function passed to withHooks.`,
+    );
   }
 }
 
 export function useState(initial) {
-  ensureCurrentInstance()
-  const id = ++callIndex
-  const state = currentInstance.$data._state
+  ensureCurrentInstance();
+  const id = ++callIndex;
+  const state = currentInstance.$data._state;
   const updater = newValue => {
-    state[id] = newValue
-  }
+    state[id] = newValue;
+  };
   if (isMounting) {
-    currentInstance.$set(state, id, initial)
+    currentInstance.$set(state, id, initial);
   }
-  return [state[id], updater]
+  return [state[id], updater];
+}
+
+export function useContext(contextName) {
+  ensureCurrentInstance();
+  return currentInstance[contextName];
+}
+
+export function useMemo(factory, deps) {
+  ensureCurrentInstance();
+  const id = ++callIndex;
+  if (!currentInstance._memoizeStore[id]) {
+    currentInstance._memoizeStore[id] = {
+      deps: [],
+      result: factory(),
+    };
+  } else {
+    const record = currentInstance._memoizeStore[id];
+    const { deps: prevDeps } = record;
+    record.deps = deps;
+    if (!deps || deps.some((d, i) => d !== prevDeps[i])) {
+      record.result = factory();
+    }
+  }
+  return currentInstance._memoizeStore[id].result;
+}
+
+export function useCallback(cb, deps) {
+  return useMemo(() => cb, deps);
 }
 
 export function useEffect(rawEffect, deps) {
-  ensureCurrentInstance()
-  const id = ++callIndex
+  ensureCurrentInstance();
+  const id = ++callIndex;
   if (isMounting) {
     const cleanup = () => {
-      const { current } = cleanup
+      const { current } = cleanup;
       if (current) {
-        current()
-        cleanup.current = null
+        current();
+        cleanup.current = null;
       }
-    }
+    };
     const effect = function() {
-      const { current } = effect
+      const { current } = effect;
       if (current) {
-        cleanup.current = current.call(this)
-        effect.current = null
+        cleanup.current = current.call(this);
+        effect.current = null;
       }
-    }
-    effect.current = rawEffect
+    };
+    effect.current = rawEffect;
 
     currentInstance._effectStore[id] = {
       effect,
       cleanup,
-      deps
-    }
+      deps,
+    };
 
-    currentInstance.$on('hook:mounted', effect)
-    currentInstance.$on('hook:destroyed', cleanup)
+    currentInstance.$on("hook:mounted", effect);
+    currentInstance.$on("hook:destroyed", cleanup);
     if (!deps || deps.length > 0) {
-      currentInstance.$on('hook:updated', effect)
+      currentInstance.$on("hook:updated", effect);
     }
   } else {
-    const record = currentInstance._effectStore[id]
-    const { effect, cleanup, deps: prevDeps = [] } = record
-    record.deps = deps
+    const record = currentInstance._effectStore[id];
+    const { effect, cleanup, deps: prevDeps = [] } = record;
+    record.deps = deps;
     if (!deps || deps.some((d, i) => d !== prevDeps[i])) {
-      cleanup()
-      effect.current = rawEffect
+      cleanup();
+      effect.current = rawEffect;
     }
   }
 }
 
 export function useRef(initial) {
-  ensureCurrentInstance()
-  const id = ++callIndex
-  const { _refsStore: refs } = currentInstance
-  return isMounting ? (refs[id] = { current: initial }) : refs[id]
+  ensureCurrentInstance();
+  const id = ++callIndex;
+  const { _refsStore: refs } = currentInstance;
+  return isMounting ? (refs[id] = { current: initial }) : refs[id];
 }
 
 export function useData(initial) {
-  const id = ++callIndex
-  const state = currentInstance.$data._state
+  const id = ++callIndex;
+  const state = currentInstance.$data._state;
   if (isMounting) {
-    currentInstance.$set(state, id, initial)
+    currentInstance.$set(state, id, initial);
   }
-  return state[id]
+  return state[id];
 }
 
 export function useMounted(fn) {
-  useEffect(fn, [])
+  useEffect(fn, []);
 }
 
 export function useDestroyed(fn) {
-  useEffect(() => fn, [])
+  useEffect(() => fn, []);
 }
 
 export function useUpdated(fn, deps) {
-  const isMount = useRef(true)
+  const isMount = useRef(true);
   useEffect(() => {
     if (isMount.current) {
-      isMount.current = false
+      isMount.current = false;
     } else {
-      return fn()
+      return fn();
     }
-  }, deps)
+  }, deps);
 }
 
 export function useWatch(getter, cb, options) {
-  ensureCurrentInstance()
+  ensureCurrentInstance();
   if (isMounting) {
-    currentInstance.$watch(getter, cb, options)
+    currentInstance.$watch(getter, cb, options);
   }
 }
 
 export function useComputed(getter) {
-  ensureCurrentInstance()
-  const id = ++callIndex
-  const store = currentInstance._computedStore
+  ensureCurrentInstance();
+  const id = ++callIndex;
+  const store = currentInstance._computedStore;
   if (isMounting) {
-    store[id] = getter()
-    currentInstance.$watch(getter, val => {
-      store[id] = val
-    }, { sync: true })
+    store[id] = getter();
+    currentInstance.$watch(
+      getter,
+      val => {
+        store[id] = val;
+      },
+      { sync: true },
+    );
   }
-  return store[id]
+  return store[id];
 }
 
 export function withHooks(render) {
   return {
     data() {
       return {
-        _state: {}
-      }
+        _state: {},
+      };
     },
     created() {
-      this._effectStore = {}
-      this._refsStore = {}
-      this._computedStore = {}
+      this._effectStore = {};
+      this._refsStore = {};
+      this._computedStore = {};
+      this._memoizeStore = {};
     },
     render(h) {
-      callIndex = 0
-      currentInstance = this
-      isMounting = !this._vnode
-      const ret = render(h, this.$attrs, this.$props)
-      currentInstance = null
-      return ret
-    }
-  }
+      callIndex = 0;
+      currentInstance = this;
+      isMounting = !this._vnode;
+      const ret = render(h, this.$attrs, this.$props);
+      currentInstance = null;
+      return ret;
+    },
+  };
 }
 
-export function hooks (Vue) {
+export function hooks(Vue) {
   Vue.mixin({
     beforeCreate() {
-      const { hooks, data } = this.$options
+      const { hooks, data } = this.$options;
       if (hooks) {
-        this._effectStore = {}
-        this._refsStore = {}
-        this._computedStore = {}
-        this.$options.data = function () {
-          const ret = data ? data.call(this) : {}
-          ret._state = {}
-          return ret
-        }
+        this._effectStore = {};
+        this._refsStore = {};
+        this._computedStore = {};
+        this._memoizeStore = {};
+        this.$options.data = function() {
+          const ret = data ? data.call(this) : {};
+          ret._state = {};
+          return ret;
+        };
       }
     },
     beforeMount() {
-      const { hooks, render } = this.$options
+      const { hooks, render } = this.$options;
       if (hooks && render) {
-        this.$options.render = function(h) {
-          callIndex = 0
-          currentInstance = this
-          isMounting = !this._vnode
-          const hookProps = hooks(this.$props)
-          Object.assign(this._self, hookProps)
-          const ret = render.call(this, h)
-          currentInstance = null
-          return ret
+        this._currentHooks = hooks;
+        if (!this._hooked) {
+          this._hooked = true;
+          this.$options.render = function(h) {
+            callIndex = 0;
+            currentInstance = this;
+            isMounting = !this._vnode;
+            const hookProps = this._currentHooks(this.$props, this);
+            Object.assign(this._self, hookProps);
+            const ret = render.call(this, h);
+            currentInstance = null;
+            return ret;
+          };
         }
       }
-    }
-  })
+    },
+  });
 }
